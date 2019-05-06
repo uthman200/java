ef510882e12d0723b9c5359bc1645aa4b78b80b6
uthman200 <uthmana200@gmail.com>
2019-05-06 13:06:58 +0100

    labs

---
+++ Labfour/Circle.java
@@ -0,0 +1,19 @@
+
+public class Circle
+{
+private static int numberOfCircles = 0;
+private double radius;
+public Circle(double circleRadius) {
+numberOfCircles++;
+radius = circleRadius;
+}
+public double area() {
+return Math.PI * radius * radius;
+}
+public double circumference() {
+return 2.0 * Math.PI * radius;
+}
+public static int getNumberOfCircles() {
+return numberOfCircles;
+}
+}
